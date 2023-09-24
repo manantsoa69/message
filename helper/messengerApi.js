a2bf549eb518521f02fcb109474fc20112711510
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-09-24 16:20:44 +0300

    add

---
+++ helper/messengerApi.js
@@ -0,0 +1,77 @@
+// helper/messengerApi.js
+const axios = require('axios');
+const NodeCache = require('node-cache');
+require('dotenv').config();
+
+// Create a new instance of NodeCache
+const tokenCache = new NodeCache();
+
+// Function to get the cached token
+const getCachedToken = () => {
+  const cachedToken = tokenCache.get('TOKEN');
+  if (cachedToken) {
+    return cachedToken;
+  }
+  // If not cached, retrieve from process.env and cache it
+  const TOKEN = process.env.TOKEN;
+  tokenCache.set('TOKEN', TOKEN);
+  return TOKEN;
+};
+
+// Function to get the cached page ID
+const getCachedPageID = () => {
+  const cachedPageID = tokenCache.get('PAGE_ID');
+  if (cachedPageID) {
+    return cachedPageID;
+  }
+  // If not cached, retrieve from process.env and cache it
+  const PAGE_ID = process.env.PAGE_ID;
+  tokenCache.set('PAGE_ID', PAGE_ID);
+  return PAGE_ID;
+};
+
+// Create a single HTTP client instance and reuse it
+const apiClient = axios.create({
+  baseURL: 'https://graph.facebook.com/v11.0/',
+});
+
+// Set the access token in the client instance's defaults by calling getCachedToken()
+apiClient.defaults.params = {
+  access_token: getCachedToken(),
+};
+
+const sendMessage = async (fbid, message) => {
+  try {
+    // Determine the maximum message length (you can adjust this as needed)
+    const maxMessageLength = 2000; // Example: Split messages if longer than 300 characters
+
+    if (message.length <= maxMessageLength) {
+      // Message is within the length limit, send it as is
+      await sendSingleMessage(fbid, message);
+    } else {
+      // Message is too long, split it into two parts and send separately
+      const part1 = message.substring(0, maxMessageLength);
+      const part2 = message.substring(maxMessageLength);
+
+      await sendSingleMessage(fbid, part1);
+      await sendSingleMessage(fbid, part2);
+    }
+    return 1;
+  } catch (error) {
+    console.error('Error occurred while sending message:', error);
+    return 0;
+  }
+};
+
+const sendSingleMessage = async (fbid, message) => {
+  return apiClient.post(`${getCachedPageID()}/messages`, {
+    recipient: { id: fbid },
+    messaging_type: 'RESPONSE',
+    message: { text: message },
+  });
+};
+
+module.exports = {
+  sendMessage,
+};
+
---
+++ routes/fbWebhookRoute.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
+const axios = require('axios'); // Import axios
+
+const { sendMessage } = require('../helper/messengerApi');
+
+async function callChatCompletionService(prompt, fbid) {
+  try {
+    const complexionServiceUrl = 'https://response-qqh1.onrender.com//generate-response';
+
+    const response = await axios.post(
+      complexionServiceUrl,
+      { prompt, fbid },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    return response.data; // Assuming the service responds with JSON data
+  } catch (error) {
+    console.error('Error calling chat completion service:', error);
+    throw error;
+  }
+}
+
+// Handle GET requests for verification
+router.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token && mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { entry } = req.body;
+    if (entry && entry.length > 0 && entry[0].messaging && entry[0].messaging.length > 0) {
+      const { sender: { id: fbid }, message } = entry[0].messaging[0];
+      if (message && message.text) {
+        let { text: query } = message;
+        console.log(`${fbid}`);
+
+        // Make the call to callChatCompletionService asynchronous
+        try {
+          const result = await callChatCompletionService(query, fbid);
+          await sendMessage(fbid, result.response);
+          res.status(200).send('OK');
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+        }
+      } else {
+        res.status(200).send('OK');
+      }
+    } else {
+      res.status(200).send('OK');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = {
+  router
+};
